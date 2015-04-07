02c8dc9e9190934960df8013b6cca3fa4201134f
Stan Zhang <stan.zhang2@gmail.com>
2015-04-06 21:53:34 -0400

    better build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,76 @@ module.exports = function(grunt) {
     grunt.initConfig({
         watch: {
             less: {
-                files: ['style/*.less'],
-                tasks: ['less']
+                files: ['src/style/*.less'],
+                tasks: ['less:src']
+            },
+            go: {
+                files: ['*.go'],
+                tasks: ['go:build:hackmann']
+            }
+        },
+        
+        copy: {
+            dist: {
+                expand: true,
+                cwd: 'src/',
+                src: ['*.html'],
+                dest: 'dist/'
             }
         },
 
         less: {
-            dist: {
+            src: {
                 options: {
-                    style: 'compressed'
+                    paths: 'src/'
                 },
                 files: {
-                    'style/style.css': 'style/importer.less'
+                    'src/style/style.css': 'src/style/importer.less'
+                }
+            },
+            dist: {
+                options: {
+                    paths: 'src/'
+                },
+                files: {
+                    'src/style/style.css': 'src/style/importer.less'
                 }
             }
-        }
+        },
+        
+        cssmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['style/*.css'],
+                    dest: 'dist/',
+                    ext: '.css'
+                }]
+            }
+        },
+        
+        uglify: {
+            dist: {
+                files: {
+                    'dist/script/script.js': ['src/script/script.js']
+                }
+            }
+        },
+        
+        go: {
+            hackmann: {
+                output: 'bin/hackmann-website.bin'
+            }
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-go');
+    grunt.registerTask('build', ['copy:dist', 'less:dist', 'cssmin:dist', 'uglify:dist'])
     grunt.registerTask('default', ['watch']);
 };
