a939e10d7a33ae23d95e5c4f957e7d534fcba893
Stan Zhang <stan.zhang2@gmail.com>
2015-04-07 18:07:07 -0400

    Style changes, bits animation refactor

--- src/script/script.js
+++ src/script/script.js
@@ -2,24 +2,23 @@ window.onload = function() {
     animateBits();
 }
 
-var bitsId = 'bits';
-var bitsAnimateInterval = 50.0;
-var bitsAnimateDuration = 1500.0;
 function animateBits() {
-    var animateBitsId = window.setInterval(changeBitsText, bitsAnimateInterval);
+    const BITS_ID = 'bits';
+    const BITS_TEXT = 'bits';
+    const BITS_ANIMATE_INTERVAL = 50.0;
+    const BITS_ANIMATE_DURATION = 1500.0;
+    var bitsTextTicks = 0;
+    var animateBITS_ID = window.setInterval(function() {
+        var len = Math.floor(bitsTextTicks / (BITS_ANIMATE_DURATION / BITS_ANIMATE_INTERVAL) * 4);
+        var text = BITS_TEXT;
+        for (i = len; i < 4; i++) {
+            text = text.substr(0, i) + (Math.random() > 0.5 ? '1' : '0') + text.substr(i + 1);
+        }
+        document.getElementById(BITS_ID).innerHTML = text;
+        bitsTextTicks++;
+    }, BITS_ANIMATE_INTERVAL);
     window.setTimeout(function() {
-        window.clearInterval(animateBitsId);
-        document.getElementById(bitsId).innerHTML = 'bits';
-    }, bitsAnimateDuration);
-}
-
-var bitsTextTicks = 0;
-function changeBitsText() {
-    var len = Math.floor(bitsTextTicks / (bitsAnimateDuration / bitsAnimateInterval) * 4);
-    var text = 'bits';
-    for (i = len; i < 4; i++) {
-        text = text.substr(0, i) + (Math.random() > 0.5 ? '1' : '0') + text.substr(i + 1);
-    }
-    document.getElementById(bitsId).innerHTML = text;
-    bitsTextTicks++;
+        window.clearInterval(animateBITS_ID);
+        document.getElementById(BITS_ID).innerHTML = BITS_TEXT;
+    }, BITS_ANIMATE_DURATION);
 }
